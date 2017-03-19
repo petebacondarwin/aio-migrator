eca0babdbdf30565fdcbd0ba2184c4789a7f764b
Peter Bacon Darwin <pete@bacondarwin.com>
2017-03-19 17:51:09 +0000

    feat(content): read cheatsheet content from local file
    We don't want the cheatsheet from the
    angular.io project. We want to read
    a local file and use that instead.

---
+++ processors/ReadContentFiles.ts
@@ -0,0 +1,27 @@
+import {DocCollection, Processor} from 'dgeni';
+import {Block} from '../pug-interfaces';
+import {ContentDocument} from '../Document';
+
+import {readFileSync} from 'fs';
+import {resolve, relative, basename, extname} from 'path';
+const glob = require('glob');
+
+export class ReadContentFilesProcessor implements Processor {
+  name = 'readContentFilesProcessor';
+  sourcePattern: string;
+  sourceBase: string;
+
+  $process(docs: DocCollection) {
+    const paths: string[] = glob.sync(resolve(this.sourceBase, this.sourcePattern));
+    paths.forEach(filePath => {
+      const contents = readFileSync(filePath, 'utf8');
+      try {
+        const baseName = basename(filePath, extname(filePath));
+        const doc = new ContentDocument(filePath, relative(this.sourceBase, filePath), baseName, contents);
+        docs.push(doc);
+      } catch (e) {
+        console.log('Failed to process', filePath, e);
+      }
+    });
+  }
+}
